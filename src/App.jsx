c64ac8f6b0ac3f87602d16a68bc71d31d9fa26b5
Samikshya2002 <shrestha.samikshya2002@gmail.com>
2025-03-05 18:35:39 +0545

    Fixed import issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import { MovieProvider } from "./contexts/MovieContext.jsx";
-import NavBar from "./components/Navbar.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 function App() {
   return (
